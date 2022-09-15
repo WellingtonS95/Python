52f28312a5e7d84d4d28c76922cd0a22895997a5
Wellington Barboza <111764526+WellingtonS95@users.noreply.github.com>
2022-09-15 18:38:04 -0300

    Update #Lista simples 001.py

--- Exercícios simples/#Lista simples 001.py
+++ Exercícios simples/#Lista simples 001.py
@@ -1,4 +1,7 @@
 #Lista simples 001
 
-lista=(2,3,5,7,8,12)
+lista=[2,3,5,7,8,12]
+lista[2]=6  #Aqui trocamos o segundo numero por 6
+lista.append(13) #Aqui adicionamos um ultimo número (13)
+lista.sort(reverse=True) #Aqui colocamos em ordem e em decrescente.
 print(lista)
