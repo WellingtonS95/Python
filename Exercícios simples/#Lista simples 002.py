0bbe6fefd27d41cb0367279d7ee35a905227b1f9
Wellington Barboza <111764526+WellingtonS95@users.noreply.github.com>
2022-09-15 19:05:01 -0300

    #Lista 002

---
+++ Exercícios simples/#Lista simples 002.py
@@ -0,0 +1,8 @@
+#Lista incrementada pelo teclado
+valores = []
+valores.append(12)
+valores.append(9)
+valores.append(4)
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print("Chegamos ao final.")
