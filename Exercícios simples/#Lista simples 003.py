dc63f1106bbd97a49ffb8b0db6e96543f66db60d
Wellington Barboza <111764526+WellingtonS95@users.noreply.github.com>
2022-09-15 19:22:54 -0300

    #Lista simples 003

---
+++ Exercícios simples/#Lista simples 003.py
@@ -0,0 +1,6 @@
+a = [2, 3, 5, 6]
+b=a[:]        #Cópia de "a" dentro de "b"
+b[2] = 8    #Troca-se o segundo numero (5) por oito.
+print(f'Os numeros da lista a são: {a}')
+print(f'Os numeros da segunda lista, a "b" são:{b}')
+
